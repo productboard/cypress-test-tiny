ac05fc8ba138125c3907d9badc530eb3562824e1
Vojta Kudrnac <vojtech.kudrnac@productboard.com>
2020-09-24 15:42:27 +0200

    pb-editor-click-fail

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://pbtest.productboard.com');
+    cy.get('[data-cy="LoginForm-EmailField"]').type('');
+    cy.get('[data-cy="LoginForm-PasswordField"]').type('');
+    cy.get('[data-cy="LoginForm-Login-Button"]').click();
+
+    cy.get('[data-cy="AppHeader-Logo"]').should('be.visible', { timeout: 60000 });
+
+    cy.get('[data-cy="NavigationMenu-Features"]').click();
+
+    cy.get('[data-cy="FeaturesBoard-FeatureTitle-Cell"]').first().click();
+
+    cy.get('[data-slate-editor="true"]').first().click();
   })
 })
